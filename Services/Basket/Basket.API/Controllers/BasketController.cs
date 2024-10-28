f4154e26f5ffed5b55491bb58d291112f4dd3443
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-10-28 15:44:16 +0700

    Basket API

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using Basket.API.Dtos;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using static Basket.API.Dtos.ApiResponse;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/basket")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly BasketRepository basketRepository;
+
+        public BasketController(BasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var carts = await basketRepository.GetCartsAsync();
+
+            var apiResponse = new APISucessResponse(
+                    statusCode: HttpStatusCode.OK,
+                    message: "Successfully get all carts",
+                    data: carts
+                );
+
+            return Ok(apiResponse);
+        }
+
+        [HttpGet]
+        [Route("{userId:Guid}")]
+        public async Task<IActionResult> GetByUserId([FromRoute] String userId)
+        {
+            var carts = await basketRepository.GetCartsByUserIdAsync(Guid.Parse(userId));
+
+            if (carts == null)
+            {
+                return NotFound();
+            }
+
+            var apiResponse = new APISucessResponse(
+                statusCode: HttpStatusCode.OK,
+                message: "Successfully get cart by userId",
+                data: carts
+            );
+
+            // Map Domain Model to DTO
+            return Ok(apiResponse);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CartDto cartDto)
+        {
+            // Map DTO to Domain Model
+            var cart = await basketRepository.CreateAsync(cartDto);
+
+            var apiResponse = new APISucessResponse(
+                statusCode: HttpStatusCode.Created,
+                message: "Successfully created cart",
+                data: cart
+            );
+
+            // Map Domain model to DTO
+            return Ok(apiResponse);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] CartDto cartDto)
+        {
+            // Map DTO to Domain Model
+            var cart = await basketRepository.UpdateAsync(id, cartDto);
+
+            var apiResponse = new APISucessResponse(
+                statusCode: HttpStatusCode.OK,
+                message: "Successfully updated cart",
+                data: cart
+            );
+
+            // Map Domain model to DTO
+            return Ok(apiResponse);
+        }
+
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var cart = await basketRepository.DeleteAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            var apiResponse = new APISucessResponse(
+                statusCode: HttpStatusCode.OK,
+                message: "Successfully deleted cart",
+                data: cart
+            );
+
+            return Ok(apiResponse);
+        }
+    }
+}
--- Services/Basket/Basket.API/Repositories/BasketRepository.cs
+++ Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -1,5 +1,8 @@
 ﻿using Basket.API.Data;
+using Basket.API.Dtos;
+using Basket.API.Models;
 using Catalog.Grpc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Basket.API.Repositories
 {
@@ -12,6 +15,115 @@ namespace Basket.API.Repositories
         {
             this.dbContext = dbContext;
             this.catalogProtoco = catalogProtoco;
+        }
+
+        // get
+        public async Task<List<Cart>> GetCartsAsync()
+        {
+            List<Cart> carts = await dbContext.Carts
+                .Include(x => x.Items)
+                .ToListAsync();
+            return carts;
+        }
+
+        // get by userId
+        public async Task<List<Cart>> GetCartsByUserIdAsync(Guid userId)
+        {
+            var carts = await dbContext.Carts
+                .Where(x => x.UserId == userId)
+                .Include(x => x.Items)
+                .ToListAsync();
+            return carts;
+        }
+
+        // update
+        public async Task<Cart?> UpdateAsync(Guid id, CartDto cartDto)
+        {
+            var existingCart = await dbContext.Carts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingCart == null)
+            {
+                return null;
+            }
+
+            // get items from catalog service
+            Cart cart = new Cart();
+            List<CartItem> cartItems = new List<CartItem>();
+            foreach (var item in cartDto.Items)
+            {
+                var productId = item.ProductId;
+                // get Product from catalog service
+                var request = new GetProductByIdRequest();
+                request.ProductId = productId.ToString();
+                var product = await catalogProtoco.GetProductByIdAsync(request);
+
+                // add item to items
+                CartItem cartItem = new CartItem()
+                {
+                    ProductId = productId,
+                    ProductName = product.Name,
+                    UnitPrice = product.Price,
+                    ImageUrl = product.ImageUrl,
+                    Quantity = item.Quantity
+                };
+                cartItems.Add(cartItem);
+            }
+
+            // map dto to domain model
+            existingCart.UserId = cartDto.UserId;
+            existingCart.Items = cartItems;
+
+            await dbContext.SaveChangesAsync();
+            return existingCart;
+        }
+
+        // add
+        public async Task<Cart> CreateAsync(CartDto cartDto)
+        {
+            // get items from catalog service
+            Cart cart = new Cart();
+            List<CartItem> cartItems = new List<CartItem>();
+            foreach (var item in cartDto.Items)
+            {
+                var productId = item.ProductId;
+                // get Product from catalog service
+                var request = new GetProductByIdRequest();
+                request.ProductId = productId.ToString();
+                var product = await catalogProtoco.GetProductByIdAsync(request);
+
+                // add item to items
+                CartItem cartItem = new CartItem()
+                {
+                    ProductId = productId,
+                    ProductName = product.Name,
+                    UnitPrice = product.Price,
+                    ImageUrl = product.ImageUrl,
+                    Quantity = item.Quantity
+                };
+                cartItems.Add(cartItem);
+            }
+
+            // map dto to domain model
+            cart.UserId = cartDto.UserId;
+            cart.Items = cartItems;
+
+            await dbContext.Carts.AddAsync(cart);
+            await dbContext.SaveChangesAsync();
+            return cart;
+        }
+        // delete
+        public async Task<Cart?> DeleteAsync(Guid id)
+        {
+            var existingCart = await dbContext.Carts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingCart == null)
+            {
+                return null;
+            }
+
+            dbContext.Carts.Remove(existingCart);
+            await dbContext.SaveChangesAsync();
+            return existingCart;
         }
     }
 }
